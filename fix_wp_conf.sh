188dcc9ba2cc908db969f9b641ac02e9f13358ee
VPS Admin <admin@vps-hp-pi.com>
2026-03-11 20:31:39 +0700

    Dọn dẹp: Chỉ giữ lại các file liên quan đến Backup Bot System

--- fix_wp_conf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /opt/vungvang-server
-sed -i "s/\\\\$\\\\$/\$_SERVER/g" docker-compose.yml
-docker compose up -d wordpress
--- sync_to_vps1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Kịch bản đồng bộ liên tục VPS2 (Chính) sang VPS1 (Phụ)
-set -e
-
-echo "=== Syncing Source Files: VPS2 -> VPS1 ==="
-
-
-echo "Sync Completed to VPS1 at $(date)"
